b96e1694a8e0b6f01ccbfa5e44afe7f449f3f43c
AndreiOprea27 <rares.oprea@lbs4.salzburg.at>
2024-12-18 15:26:32 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace Oprea.Andrei._10_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ausgabe(0);
+        }
+
+        static public void Ausgabe(int zaehler)
+        {
+            string[] verzeichnisse;
+            string test = Directory.GetCurrentDirectory();
+
+            verzeichnisse = Directory.GetDirectories(test);
+
+            if(zaehler < 5)
+            {
+                foreach (string v in verzeichnisse)
+                {
+                    Console.WriteLine(v);
+                    verzeichnisse = Directory.GetDirectories(v);
+                    Ausgabe(zaehler + 1);
+                }
+                
+            }
+            
+        }
+    }
+}
