b6faf8d120b419c279e1eed967806adf3705839e
AndreiOprea27 <rares.oprea@lbs4.salzburg.at>
2024-12-18 15:27:23 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Oprea.Andrei._10._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bitte Dateinamen der zu kopierenden Datei eingeben: ");
+            string copy = Console.ReadLine();
+            Console.WriteLine("Bitte Dateinamen der Kopie eingeben: ");
+            string paste = Console.ReadLine();
+
+            File.Copy(copy, paste);
+        }
+    }
+}
