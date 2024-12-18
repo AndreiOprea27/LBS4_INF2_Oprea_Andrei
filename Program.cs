e543075539c1494650e7293b54906ddcbb3061e7
AndreiOprea27 <rares.oprea@lbs4.salzburg.at>
2024-12-18 15:25:37 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+
+namespace Oprea.Andrei._10._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ein;
+            Console.WriteLine("Bitte den Dateinamen eingeben: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hier ist der Daten-Inhalt: ");
+
+            System.IO.FileStream lesen = new System.IO.FileStream($"{name}.txt", System.IO.FileMode.Open);
+                
+            do
+            {
+                ein = lesen.ReadByte();
+                if (ein != -1) Console.Write((char)ein);
+            }
+            while (ein != -1);
+            Console.WriteLine();
+            lesen.Close();
+        }
+    }
+}
