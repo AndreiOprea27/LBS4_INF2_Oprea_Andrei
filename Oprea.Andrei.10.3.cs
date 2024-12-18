d2e85e4a722066f65fc0cf6c6ed650c3431a8842
AndreiOprea27 <rares.oprea@lbs4.salzburg.at>
2024-12-18 16:31:37 +0100

    Update Oprea.Andrei.10.3.cs

--- Oprea.Andrei.10.3.cs
+++ Oprea.Andrei.10.3.cs
@@ -1,32 +1,25 @@
-﻿using static System.Net.Mime.MediaTypeNames;
-
 namespace Oprea.Andrei._10_3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Ausgabe(0);
+            Ausgabe("net8.0");
         }
 
-        static public void Ausgabe(int zaehler)
+        static public void Ausgabe(string folder)
         {
             string[] verzeichnisse;
-            string test = Directory.GetCurrentDirectory();
+            folder = Directory.GetCurrentDirectory();
 
-            verzeichnisse = Directory.GetDirectories(test);
+            verzeichnisse = Directory.GetDirectories(folder);
 
-            if(zaehler < 5)
+            foreach (string v in verzeichnisse)
             {
-                foreach (string v in verzeichnisse)
-                {
-                    Console.WriteLine(v);
-                    verzeichnisse = Directory.GetDirectories(v);
-                    Ausgabe(zaehler + 1);
-                }
-                
+                Console.WriteLine(v);
+                verzeichnisse = Directory.GetDirectories(folder);
+                Ausgabe(v);
             }
-            
         }
     }
 }
