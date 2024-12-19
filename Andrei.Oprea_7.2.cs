def640f3a731d77c73f1d7d4ce957c78de2cf47b
AndreiOprea27 <rares.oprea@lbs4.salzburg.at>
2024-12-19 10:13:16 +0100

    Update Andrei.Oprea_7.2.cs

--- Andrei.Oprea_7.2.cs
+++ Andrei.Oprea_7.2.cs
@@ -106,6 +106,10 @@ namespace Andrei.Oprea_7._2
     class CElefant : CTier 
     {
         public CElefant(string name, int geschwindigkeit) : base(name, geschwindigkeit) { }
+        /// <summary>
+        /// Was die Kamera Methode für den Elefant macht
+        /// </summary>
+        /// <returns>spezielle Text des Elefantes</returns>
         public override string Kamera()
         {
             return "*badet sich im Sicht der Kamera*";
@@ -114,6 +118,10 @@ namespace Andrei.Oprea_7._2
     class CNashorn : CTier
     {
         public CNashorn(string name, int geschwindigkeit) : base(name, geschwindigkeit) { }
+        /// <summary>
+        /// Was die Kamera für den Nashorn macht
+        /// </summary>
+        /// <returns>spezielle Text des Nashorns</returns>
         public override string Kamera()
         {
             return "*läuft zur Kamera*";
@@ -123,6 +131,10 @@ namespace Andrei.Oprea_7._2
     class CDelfin : CTier
     {
         public CDelfin(string name, int tauchzeit) : base(name, tauchzeit) { }
+        /// <summary>
+        /// Was die Kamera für den Delfin macht
+        /// </summary>
+        /// <returns>spezielle Text des Delfins</returns>
         public override string Kamera()
         {
             return "*spielt mit die Kamera*";
@@ -131,6 +143,10 @@ namespace Andrei.Oprea_7._2
     class CWal : CTier
     {
         public CWal(string name, int tauchzeit) : base(name, tauchzeit) { }
+        /// <summary>
+        /// Was die Kamera für die Wal macht
+        /// </summary>
+        /// <returns>spezielle Text der Wal</returns>
         public override string Kamera()
         {
             return "*ignoriert die Kamera*";
